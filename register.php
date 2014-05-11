5b823b0ba052847f4f5fd8d5832722013ea18e67
jimmymac205 <jimmymac_205@yahoo.com>
2014-05-11 09:55:06 -0500

    t

--- register.php
+++ register.php
@@ -1,36 +1,3 @@
 <?php
-// define variables and set to empty values
-$firstnameErr = $lastnameErr = $emailErr = $codeErr = $passErr = "";
-$firstname = $lastname = $email = $code = $password = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["firstname"])) {
-    $firstnameErr = "First Name is required";
-  } else {
-    $firstname = test_input($_POST["firstname"]);
-  }
-
-  if (empty($_POST["lastname"])) {
-    $lastnameErr = "Last Name is required";
-  } else {
-    $lastname = test_input($_POST["lastname"]);
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
-  
-    if (empty($_POST["password"])) {
-    $passErr = "Password is required";
-  } else {
-    $password = test_input($_POST["password"]);
-  }
-  
-  if (empty($_POST["code"])) {
-    $codeErr = "Code is required";
-  } else {
-    $code = test_input($_POST["code"]);
-  }
+echo "hi";
 ?>
